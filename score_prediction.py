d4463068148aefedbce72f4d06dd93daf97a79a5
qinzhiguo <qinzhiguohit@gmail.com>
2017-11-05 19:37:17 +0800

    Refine the output and commment out unessential debug info

--- score_prediction.py
+++ score_prediction.py
@@ -10,7 +10,7 @@ def predictScore(team1_id,team2_id):
     db_con = sqlite3.connect("database.sqlite")
     score_match_history = pd.read_sql_query("select season,home_team_api_id,away_team_api_id,home_team_goal,away_team_goal from Match where home_team_api_id= %s  and away_team_api_id= %s"  % (team1_id,team2_id), db_con)
 
-    print("Team Versus History : %s" % score_match_history)
+    #print("Team Versus History : %s" % score_match_history)
 
     #Need update the column name when the data format is ready
     team1 = score_match_history[['home_team_goal']]
@@ -23,7 +23,7 @@ def predictScore(team1_id,team2_id):
     score_y = round(score_y)
 
     print("----------------------")
-    print("Score  = %s : %s " %(score_x,score_y))
+    print("Predict Score  = %s : %s " %(score_x,score_y))
     print("----------------------")
 
     #getScoreHistoryByTeam(team1_id,team2_id)
@@ -33,7 +33,7 @@ def predictScore(team1_id,team2_id):
     return predict_result
 
 if __name__ == '__main__':
-    predictScore(8634,8315)
+    predictScore(8634,8633)
 
 
 
