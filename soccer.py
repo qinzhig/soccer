d201ae736dbb39ac5ba4dc04d7d93c880da040ce
huazhihao <i@huazhihao.com>
2017-11-05 15:52:36 +0800

    Add match (score_gap,player_id,hda,player_overall_rating)

--- soccer.py
+++ soccer.py
@@ -66,12 +66,15 @@ def match():
     d = {}
     for r in rs:
         d[r[0]] = r[1]
+    hda = "BWH,BWD,BWA,IWH,IWD,IWA,LBH,LBD,LBA,PSH,PSD,PSA,WHH,WHD,WHA,SJH,SJD,SJA,VCH,VCD,VCA,GBH,GBD,GBA,BSH,BSD,BSA"
 
-    sql = "select home_team_goal-away_team_goal, {0}, {1} from match where ({2} and {3});".format(
+    sql = "select home_team_goal-away_team_goal, {0}, {1}, {4} from match where ({2} and {3} and {5});".format(
         ",".join(map((lambda i: "home_player_%s" % i), range(1, 12))),
         ",".join(map((lambda i: "away_player_%s" % i), range(1, 12))),
         " and ".join(map((lambda i: "home_player_%s is not null" % i), range(1, 12))),
         " and ".join(map((lambda i: "away_player_%s is not null" % i), range(1, 12))),
+        hda,
+        " and ".join(map((lambda i: "%s is not null" % i), hda.split(","))),
         )
     print("sql:" + sql)
     c.execute(sql)
